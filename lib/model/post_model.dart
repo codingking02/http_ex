d0b8f138f14615082ef58b802fbaca5784df37c5
codingking02 <zeyadismailo2002@gmail.com>
2023-07-02 17:39:51 +0300

    ch

---
+++ lib/model/post_model.dart
@@ -0,0 +1,21 @@
+class Post {
+  int userId;
+  int id;
+  String title;
+  String body;
+  Post({
+    required this.body,
+    required this.id,
+    required this.title,
+    required this.userId,
+  });
+  factory Post.fromJson(Map<String, dynamic> _map) {
+    Post _post = Post(
+      body: _map["body"],
+      id: _map["id"],
+      title: _map["title"],
+      userId: _map["userId"],
+    );
+    return _post;
+  }
+}
--- lib/network/http_connection.dart
+++ lib/network/http_connection.dart
@@ -1,16 +1,29 @@
+import 'dart:convert';
 import 'package:http/http.dart' as varHttp;
 import 'package:http/http.dart';
+import 'package:http_ex/model/post_model.dart';
 
 class HttpConnections {
   static String baseUrl = "https://jsonplaceholder.typicode.com/";
   static String postEndPoint = "posts";
   final client = varHttp.Client();
 
-  void fetchPost(int id) async {
+  Future<Post> fetchPost(int id) async {
     Response response = await client.get(
       Uri.parse(baseUrl + postEndPoint + '/$id'),
     );
 
-    if (response.statusCode == 200) {}
+    if (response.statusCode == 200) {
+      String body = response.body;
+      Map<String, dynamic> _map =
+          jsonDecode(body); // 1- return string to json // 2- json to map
+      // 3- map to dart data
+      Post _post = Post.fromJson(
+        _map,
+      );
+      return _post;
+    } else {
+      throw Exception("failed to load posts $id");
+    }
   }
 }
